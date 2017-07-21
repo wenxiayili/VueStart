d7142d39d78971e2b2dd62d5e3b550faf3700a34
李运华 <yunhuahesunjing@live.com>
2017-07-21 09:23:23 +0800

    初始化项目
    初始化项目提交

---
+++ src/config/index.js
@@ -0,0 +1,7 @@
+let host = process.env.NODE_ENV === 'production' ? 'http://bugs.hitsoft.com.cn:8001' : 'http://localhost:8086'
+let api = process.env.NODE_ENV === 'production' ? 'http://bugs.hitsoft.com.cn:8001/api' : 'http://localhost:8086/api'
+export default{
+  host: host,
+  api: api,
+  appid: 'wx4dacd03ae6cf5ec2'
+}
---
+++ src/main.js
@@ -0,0 +1,66 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import FastClick from 'fastclick'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+import Vuex from 'Vuex'
+import moment from 'moment'
+import config from './config'
+import App from './App'
+import Home from './components/HelloFromVux'
+
+Vue.use(VueRouter)
+Vue.use(VueResource)
+Vue.use(Vuex)
+Vue.prototype.$moment = moment
+
+// vue-route
+const routes = [{
+  path: '/',
+  component: Home
+}]
+
+const router = new VueRouter({
+  routes
+})
+
+// Vuex
+const store = new Vuex.Store({
+  state: {
+    token: JSON.parse(window.sessionStorage.getItem('token'))
+  },
+  mutations: {
+    setToken (state, token) {
+      state.token = token
+      window.sessionStorage.setItem('token', JSON.stringify(token))
+    }
+  }
+})
+// vue-resource
+Vue.http.options.root = config.api
+Vue.http.options.before = r => {
+  var token = store.state.token
+  if (r.url !== 'token' && token && token.access_token) {
+    r.headers.append('Authorization', token.token_type + ' ' + token.access_token)
+  }
+}
+Vue.http.interceptors.push((request, next) => {
+  next(response => {
+    if (response.status === 401) {
+      router.replace('/login')
+    }
+  })
+})
+FastClick.attach(document.body)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+var vm = new Vue({
+  router,
+  render: h => h(App),
+  store
+})
+window.vm = vm
+vm.$mount('#app-box')
